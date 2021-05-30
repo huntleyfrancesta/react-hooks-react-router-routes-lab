c3329a1533848af6573540808c77399ddadb8652
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-05-29 22:00:04 -0400

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+const linkStyles = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px"
 }
+
+function NavBar() {
+  return (
+  <div>
+    <NavLink to="/" exact style={linkStyles}>
+      Home
+    </NavLink>
+    <NavLink to ="/movies" exact style={linkStyles}>
+      Movies
+    </NavLink>
+    <NavLink to="/directors" exact style={linkStyles}>
+      Directors
+    </NavLink>
+    <NavLink to="/actors" exact style={linkStyles}>
+      Actors
+    </NavLink>
+  </div>
+  )};
 
 export default NavBar;
